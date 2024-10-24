765848dd9a0e39e841595ea8cf1085314b0666be
DaeSoeps <thdeotjq12@naver.com>
2024-10-24 23:33:05 +0900

    FEAT: DTO 구현

--- backend/src/boards/boards.controller.ts
+++ backend/src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,5 +10,17 @@ export class BoardsController {
     @Get()
     getAllBoard(): Board[]{
         return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() CreateBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardsService.createBoard(CreateBoardDto)
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string):Board{
+        return this.boardsService.getBoardById(id);
     }
 }
--- backend/src/boards/boards.service.ts
+++ backend/src/boards/boards.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
 
 // 게시물에 관한 로직을 처리하는곳 Service -> Controller(에서 서비스를 불러와 사용)
 @Injectable()
@@ -12,7 +14,8 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string){
+    createBoard(createBoardDto: CreateBoardDto){
+        const {title, description} = createBoardDto;
         const board: Board = {
             id: uuid(),
             title,
@@ -20,8 +23,13 @@ export class BoardsService {
             status: BoardStatus.PUBLIC
         }
 
-        this.boards.push(board)
+        this.boards.push(board);
         return board;
     }
+
+    getBoardById(id: string): Board{
+        return this.boards.find((board) => board.id === id)
+    }
+
 }
 
