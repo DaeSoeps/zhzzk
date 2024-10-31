17616a795fe1774af1187bfd9132af62555886ef
DaeSoeps <thdeotjq12@naver.com>
2024-10-31 21:26:55 +0900

    FEAT: 게시물 가져오기, 삭제시 exception 메시지 추가.

--- backend/src/boards/boards.service.ts
+++ backend/src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -28,11 +28,17 @@ export class BoardsService {
     }
 
     getBoardById(id: string): Board{
-        return this.boards.find((board) => board.id === id)
+        const found = this.boards.find((board) => board.id === id);
+        if(!found){
+            throw new NotFoundException(`해당 아이디를 찾을 수 없습니다. ${id}`);
+        }
+
+        return found;
     }
 
     deleteBoard(id: string): void{
-        this.boards = this.boards.filter((board)=> board.id !== id);
+        const found = this.getBoardById(id);
+        this.boards = this.boards.filter((board)=> board.id !== found.id);
     }
 
     updateBoardStatus(id: string, status: BoardStatus): Board {
