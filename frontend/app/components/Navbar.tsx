2015c71e56478623f1c95ead910d8a97eb06c372
DaeSoeps <thdeotjq12@naver.com>
2024-11-06 22:41:48 +0900

    FEAT: 디자인 수정

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -4,6 +4,7 @@
 import { useState } from 'react';
 import { FiSearch, FiLogOut, FiUser, FiChevronDown } from 'react-icons/fi';
 import { BsFillPersonFill } from 'react-icons/bs';
+import Link from 'next/link'
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -56,12 +57,14 @@ const Navbar = () => {
                 <FiUser className="mr-2" /> 내 채팅
               </li>
               <hr className="border-gray-600 my-1" />
-              <li
-                onClick={handleLogout}
-                className="px-4 py-2 hover:bg-gray-600 cursor-pointer flex items-center text-red-400"
-              >
-                <FiLogOut className="mr-2" /> 로그아웃
-              </li>
+              <Link href="/login">
+                <li
+                  onClick={handleLogout}
+                  className="px-4 py-2 hover:bg-gray-600 cursor-pointer flex items-center text-red-400"
+                >
+                  <FiLogOut className="mr-2" /> 로그아웃
+                </li>
+              </Link>
             </ul>
           </div>
         )}
