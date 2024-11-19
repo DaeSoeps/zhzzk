75b4cbad0128db25a0dad5cc00ae88b59dc2383b
DaeSoeps <thdeotjq12@naver.com>
2024-11-20 02:28:10 +0900

    FIX: 동적 라우트 경로 에러 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/:path*', // 프론트엔드 요청 경로
+        source: '/api/:path*', // 프론트엔드 요청 경로
         destination: 'https://api.chzzk.naver.com/:path*', // 외부 API 주소
       },
     ];
