b3c60670faa580a1bb9193d2ae275e7b19d6ea29
DaeSoeps <thdeotjq12@naver.com>
2024-11-23 01:07:03 +0900

    FIX: 스트리밍 영역 css 수정

--- frontend/app/broadcast/page.tsx
+++ frontend/app/broadcast/page.tsx
@@ -7,14 +7,16 @@ const BroadcastPage: React.FC = () => {
     return (
 
         <Layout>
-            {/* 가운데 메인 콘텐츠: 플레이어 */}
-            <div className="flex flex-1 justify-center items-center">
-                <StreamPlayer isBroadcastMode />
-            </div>
+            <div className="flex w-full h-full">
+                {/* 가운데 메인 콘텐츠: 플레이어 */}
+                <div className="flex flex-1 justify-center items-center w-full h-full">
+                    <StreamPlayer isBroadcastMode />
+                </div>
 
-            {/* 오른쪽 채팅 영역 */}
-            <div className="w-[300px] bg-gray-800 p-4 h-full overflow-y-auto">
-                <ChatRoomBroadCast isBroadcastMode />
+                {/* 오른쪽 채팅 영역 */}
+                <div className="w-[300px] bg-gray-800 p-4 h-full overflow-y-auto">
+                    <ChatRoomBroadCast isBroadcastMode />
+                </div>
             </div>
         </Layout>
 
--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="text-gray-400 py-4 px-6 px-4 shadow-lg">
-      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
+      <div className="container mx-auto flex flex-col sm:flex-row justify-end mx-0 items-center">
         {/* 저작권 정보 */}
-        <p className="text-sm">
+        <p className="text-sm mx-6">
           copyright © {new Date().getFullYear()} My Streaming App. All rights reserved by DaeSoeps.
         </p>
-        <p className="text-sm">
+        <p className="text-sm mx-6">
         Version 0.0.1
         </p>
         {/* 출처 및 링크 */}
-        <div className="flex space-x-4 mt-2 sm:mt-0">
+        <div className="flex space-x-4 mt-2 sm:mt-0 mx-6">
           <a
             href="https://icons8.kr"
             target="_blank"
