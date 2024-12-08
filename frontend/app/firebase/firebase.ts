e20bc85428bb64a67512e46ecc02c13b57060105
DaeSoeps <thdeotjq12@naver.com>
2024-12-09 05:13:28 +0900

    FIX: firebase 함수추가

--- frontend/app/firebase/firebase.ts
+++ frontend/app/firebase/firebase.ts
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.FB_API_KEY,
   authDomain: "zhzzk-36b49.firebaseapp.com",
+  databaseURL: "https://zhzzk-36b49-default-rtdb.firebaseio.com",
   projectId: "zhzzk-36b49",
   storageBucket: "zhzzk-36b49.firebasestorage.app",
   messagingSenderId: "312975719986",
@@ -11,5 +13,14 @@ const firebaseConfig = {
   measurementId: "G-7L8CWJ5YN3"
 };
 
+// database
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
+
+// firestore
+const firestore = getFirestore(app);
+export const firebaseDB = firestore;
+
+// export const auth = getAuth(app);       // for 인증
+// export const storage = getStorage(app); // for 스토리지
+// export const db = getFirestore(app);    // for 데이터베이스
--- frontend/app/firebase/firebaseActions.ts
+++ frontend/app/firebase/firebaseActions.ts
@@ -1,10 +1,47 @@
-import { ref, set, onValue } from "firebase/database";
+import { ref, set, onValue, push, get } from "firebase/database";
 import { database } from "./firebase";
+import { StaticImageData } from 'next/image';
+interface Streamer {
+    id: string;
+    name: string;
+    icon: StaticImageData;
+    viewers: number;
+    game: string;
+    streamType: string;
+}
 
+// 스트리머 불러오기
+export const getStreamers = async (): Promise<Streamer[]> => {
+    const streamersRef = ref(database, "streamers");
+    const snapshot = await get(streamersRef);
+
+    if (snapshot.exists()) {
+        const data = snapshot.val();
+
+        // 데이터를 배열 형태로 변환
+        const streamersArray = Object.keys(data).map((key) => ({
+            ...(data[key]),
+        }));
+        const test = streamersArray[0]
+        const result : Streamer[] = Object.keys(test).map(el => test[el]); 
+
+        return result;
+    }
+
+    return []; // 데이터가 없으면 빈 배열 반환
+};
+// 스트리머 추가
+export const addStreamer = (streamer: object) => {
+    const streamersRef = ref(database, "streamers");
+    push(streamersRef, streamer);
+};
+
+// 스트리밍 시작
 export const startStreaming = (streamerId: string, streamerData: object) => {
     set(ref(database, `streamers/${streamerId}`), streamerData);
 };
 
+// 구독
 export const subscribeToStreamers = (callback: any) => {
     const streamersRef = ref(database, "streamers");
     onValue(streamersRef, (snapshot) => {
