f9667e032cfb383c619bf7a0dca150fca84b51a4
DaeSoeps <thdeotjq12@naver.com>
2024-12-24 03:04:29 +0900

    FIX: 동적 라우팅 오류 수정(build error)

--- frontend/app/streamer/[streamerName]/[streamType]/page.tsx
+++ frontend/app/streamer/[streamerName]/[streamType]/page.tsx
@@ -1,56 +1,28 @@
 'use client';
 import React from 'react';
-import { useParams } from 'next/navigation';
 import ChatRoom from '../../../components/ChatRoom';
 import StreamPlayer from '../../../components/StreamPlayer';
 import Layout from '../../../components/Layout';
 import LiveInfomation from '@/app/components/LiveInfomation';
-// import { GetStaticPaths } from 'next';
 
 
-// export const getStaticPaths = (async () => {
-//     return {
-//       paths: [
-//       ],
-//       fallback: "blocking", // false or "blocking"
-//     }
-//   }) satisfies GetStaticPaths
 
-
-const StreamerPage: React.FC = () => {
-    const params = useParams<{ streamerName: string; streamType: string }>();
-    console.log("PROPS : ", params)
-    const streamerName = decodeURIComponent(params.streamerName);
-    // const streamUrl = `https://example.com/hls/${streamerName}.m3u8`; // HLS URL
-
-
+export default async function StreamerPage({ params, }: {
+    params: Promise<{ streamerName: string; streamType: string }>
+}) {
     return (
         <Layout>
             {/* 가운데 메인 콘텐츠: 플레이어 */}
             <div className="flex flex-col justify-between items-center h-full w-full">
-                <StreamPlayer streamUrl={""} streamType={params.streamType} />
-                <LiveInfomation streamerName={streamerName} />
+                <StreamPlayer streamUrl={""} streamType={(await params).streamType} />
+                <LiveInfomation streamerName={decodeURIComponent((await params).streamerName)} />
             </div>
-
+ 
             {/* 오른쪽 채팅 영역 */}
             <div className="w-[400px] bg-gray-800 p-4 h-full overflow-y-auto right-0">
-                <ChatRoom streamerName={streamerName} />
+                <ChatRoom streamerName={decodeURIComponent((await params).streamerName)} />
             </div>
 
         </Layout>
-    );
-};
-
-// export const getServerSideProps: GetServerSideProps  = async ({ params }) => {
-//     const id = params;
-//     console.log("PROPS !: ", params)
-//     // 필요한 데이터를 가져와 props로 전달
-//     return {
-//         props: {
-//             id,
-//         },
-//     };
-// };
-
-
-export default StreamerPage;
+    )
+}
